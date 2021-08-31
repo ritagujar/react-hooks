872280f9730f0c55a5ca2afec6c3a2a31215dc66
Rita-Gujar <ritagujar14@gmail.com>
2021-08-31 19:51:20 +0530

    Changed file structure

---
+++ src/components/Usestate.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import "../App.css";
+
+function Usestate() {
+  const [name, setName] = useState("React");
+  // Use Boolean in the useState
+  const [flag, setFlag] = useState(false);
+  // Use Numbers in the useState
+  const [steps, setSteps] = useState(0);
+  // Use Array in the useState
+  const [names, setNames] = useState([]);
+
+  const nameChange = () => {
+    console.log("Clicked");
+    // return setName("Learner");
+    return setFlag(!flag);
+  };
+
+  const increament = () => {
+    return setSteps(steps + 1);
+
+    // To increament the value by 2 use prevState which stores the previous state
+    // Use prevState in order to make use of previous state
+    // setSteps((prevState) => prevState + 1);
+    // setSteps((prevState) => prevState + 1);
+  };
+
+  const decreament = () => {
+    return setSteps(steps - 1);
+  };
+
+  const addName = (e) => {
+    e.preventDefault();
+    setNames([...names, { id: names.length, name }]);
+    setName("");
+  };
+
+  return (
+    <div className="App">
+      {/* <div>Hello, {name}</div> */}
+      <div>Hello {flag ? name : ""}</div>
+      <button onClick={nameChange}>Click me</button>
+      <hr />
+      <button onClick={increament}>+</button>
+      <div>{steps}</div>
+      <button onClick={decreament}>-</button>
+
+      <hr />
+      <form onSubmit={addName}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Add Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+      <hr />
+      <ul>
+        {names.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Usestate;
